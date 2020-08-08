577dc39933f3287767f082ca5e8817786cd0f5df
kjh03160 <kis03160@likelion.org>
2020-08-08 22:51:26 +0900

    Heap sort

--- CS_Shin/03_Sort/Heap.py
+++ CS_Shin/03_Sort/Heap.py
@@ -18,7 +18,8 @@ class Heap:
 
             if self.A[left] > self.A[k]:
                 m = left
-            elif right < n and self.A[right] > self.A[m]:
+
+            if right < n and self.A[right] > self.A[m]:
                 m = right
 
             if m == k:
@@ -26,14 +27,30 @@ class Heap:
             self.A[k], self.A[m] = self.A[m], self.A[k]
             k = m
 
+    def heapify_up(self, k):
+        while k > 0 and self.A[(k - 1) // 2] < self.A[k]:
+            self.A[(k - 1) // 2], self.A[k] = self.A[k], self.A[(k - 1) // 2]
+            k = (k - 1) // 2
+
+    def insert(self, val):
+        self.A.append(val)
+        self.heapify_up(len(self.A) - 1)
+
     def make_heap(self):
         n = len(self.A)
-        for k in range(n // 2 - 1, -1, -1): # 리프노드는 자식 없으므로 할 필요 없음
+        for k in range(n - 1, -1, -1): # 리프노드는 자식 없으므로 할 필요 없음
             self.heapify_down(k, n)  # arr[k] 힙성질 만족하도록
 
     def heap_sort(self):
         n = len(self.A)
-        for k in range(n - 1, -1, -1):
+        for k in range(len(self.A) - 1, -1, -1):
             self.A[0], self.A[k] = self.A[k], self.A[0]
             n = n - 1  # A[n-1]은 정렬되었으므로
             self.heapify_down(0, n)
+
+    def delete_max(self):
+        self.A[0], self.A[-1] = self.A[-1], self.A[0]
+        x = self.A.pop()
+        self.heapify_down(0, len(self.A))
+        return x
+
